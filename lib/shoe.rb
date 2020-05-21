a4ab86225e79a41406d56e9e4728e4e8ec2505e2
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-21 02:22:38 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -13,8 +13,10 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  def brand=(brand)
-    @brand = brand
+  def initialize(brand)
+  @brand = brand
+  if BRANDS.include?(brand)
+  else
     BRANDS << brand
   end
 end
